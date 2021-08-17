7483ef0556f3284202f0892f669264f056271544
Sean Christian Lozana <lozanasc@gmail.com>
2021-08-17 22:48:16 +0800

    .js -> .jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { title as Title } from './common/headings';
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Title
+          content = "Portfolio website!"
+        />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
