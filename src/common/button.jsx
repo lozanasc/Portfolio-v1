9e6a1ac473f4be5bd88a43a931a4e2b1e731d091
Sean Christian Lozana <lozanasc@gmail.com>
2021-08-17 22:48:54 +0800

    Initial commit for common portfolio components

---
+++ src/common/button.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const button = props => <button> {props.content} </button>
---
+++ src/common/headings.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../styles/common/heading.scss';
+
+export const title = props => <h1 className="title"> {props.content} </h1>
+export const header = props => <h1 className="header"> {props.content} </h1>
