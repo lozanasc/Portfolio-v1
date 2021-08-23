e68edc0c8313c38a259ccb6b148226fe8e645e00
Sean Christian Lozana <lozanasc@gmail.com>
2021-08-23 08:56:22 +0800

    Added needed components that completes the portfolio on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { title as Title } from './common/headings';
-function App() {
+import './styles/app.scss';
+import { Element } from 'react-scroll';
+import Navigation from './views/Header';
+import Home from './views/Home';
+import About from './views/About';
+import Projects from './views/Projects';
+import Contact from './views/Contact';
+
+function Portfolio() {
   return (
     <div className="App">
-      <h1>
-        <Title
-          content = "Portfolio website!"
-        />
-      </h1>
+        <Navigation/>
+        <Element 
+          name = "Content" 
+          className="Content" 
+          id="Content"
+        >
+          <Home/>
+          <About/>
+          <Projects/>
+          <Contact/>
+        </Element>  
     </div>
   );
 }
 
-export default App;
+export default Portfolio;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App.jsx';
 import './styles/index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   document.getElementById('root')
 );
