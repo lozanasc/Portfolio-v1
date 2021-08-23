e28ff0014ccf44d801c37c9f881072612961178f
Sean Christian Lozana <lozanasc@gmail.com>
2021-08-23 09:01:42 +0800

    Completed Navigational bar: * Completed nav bar and its styling
    Signed-off-by: Sean Christian Lozana <lozanasc@gmail.com>

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
+import '../styles/navbar.scss';
+import { motion } from 'framer-motion'
+import { Link as NavLink } from 'react-scroll';
+
 
 function Header(){
     return (
-        <nav className="NavBar">
-            {/* Navigation code goes here */}
-        </nav>
+        <motion.nav 
+            initial = {{opacity: 0}}
+            animate = {{opacity: 1}}
+            transition = {{delay: 1, duration: 1.8}}
+            className="NavBar"
+        >
+            <div className="LinkContainer">
+
+                <NavLink 
+                    activeClass="active"
+                    spy={true}                
+                    smooth={true} 
+                    duration={1000} 
+                    containerId="Content" 
+                    to="Home"
+                > 
+                    HOME 
+                </NavLink>
+
+                <NavLink 
+                    activeClass="active"
+                    spy={true}        
+                    smooth={true} 
+                    duration={1000} 
+                    offset={20}
+                    containerId="Content" 
+                    to="About"
+                > 
+                    ME
+                </NavLink>
+
+                <NavLink 
+                    activeClass="active" 
+                    spy={true}     
+                    smooth={true} 
+                    duration={1000} 
+                    containerId="Content" 
+                    to="Projects"
+                > 
+                    PROJECTS 
+                </NavLink>
+
+                <NavLink 
+                    activeClass="active" 
+                    spy={true}     
+                    smooth={true} 
+                    duration={1000} 
+                    containerId="Content" 
+                    to="Contact"
+                >
+                        CONTACT
+                </NavLink>
+
+            </div>
+        </motion.nav>
     );
 }
 
